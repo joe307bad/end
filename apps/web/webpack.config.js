27e671c8f606e19441d3601f7ad379fb9fa61fa0
Joe Badaczewski <joebad@amazon.com>
2024-01-14 23:26:43 -0500

    [web] fixing webpack.config.js

--- apps/web/webpack.config.js
+++ apps/web/webpack.config.js
@@ -11,36 +11,10 @@ module.exports = composePlugins(withNx(), withReact(), (config) => {
         '../Utilities/Platform': 'react-native-web/dist/exports/Platform',
         '../../Utilities/Platform': 'react-native-web/dist/exports/Platform',
         './Platform': 'react-native-web/dist/exports/Platform',
-        '../../Image/Image': 'identity-obj-proxy',
-        '../Utilities/BackHandler': 'identity-obj-proxy',
-        '../../StyleSheet/PlatformColorValueTypes': 'identity-obj-proxy',
       },
     },
     module: {
       rules: [
-        {
-          test: /.[tj]sx?$/,
-          include: [
-            path.resolve(__dirname, 'src'),
-            path.resolve(__dirname, '../../node_modules/react-native'),
-            path.resolve(__dirname, '../../node_modules/@react-native'),
-            path.resolve(__dirname, '../../node_modules/@react-navigation'),
-            path.resolve(
-              __dirname,
-              '../../node_modules/expo-dev-menu/vendored/react-native-safe-area-context/src'
-            ),
-          ],
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                cacheDirectory: true,
-                presets: ['module:metro-react-native-babel-preset'],
-                plugins: [['react-native-web', { commonjs: true }]],
-              },
-            },
-          ],
-        },
         {
           test: /\.(scss|css)$/i,
           use: ['sass-loader'],
