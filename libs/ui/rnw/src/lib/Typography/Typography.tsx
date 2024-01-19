8a23a4e74969d102328f105f1ed0891499eb35b2
Joe Badaczewski <joebad@amazon.com>
2024-01-18 23:07:19 -0500

    Adding tsc target to components lib (#24)

--- libs/ui/rnw/src/lib/Typography/Typography.tsx
+++ libs/ui/rnw/src/lib/Typography/Typography.tsx
@@ -1,7 +1,7 @@
 import * as t from 'tamagui';
-import { ReactNode } from 'react';
-import Styles from './Typography.module.scss'
+import React, { ReactNode } from 'react';
+import Styles from './Typography.module.scss';
 
 export function H1({ children }: { children?: ReactNode }): ReactNode {
-  return <t.H1 id={Styles.h1}>{children}</t.H1>;
+  return <t.H1 id={Styles['h1']}>{children}</t.H1>;
 }
