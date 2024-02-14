e7cd1f4543840da866acbf173cc23ca6a97024f6
Joe Badaczewski <joebad@amazon.com>
2024-02-14 14:45:34 -0500

    [nest + libs/ui] basic auth with nest + loading button

---
+++ apps/nest/src/app/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthGuard } from './auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.NEST_JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
+  controllers: [AuthController],
+})
+export class AuthModule {}
--- libs/ui/rnw/src/lib/Landing/Landing.tsx
+++ libs/ui/rnw/src/lib/Landing/Landing.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import * as Typography from '../Typography';
-import { Input, XStack, Button, YStack } from 'tamagui';
+import { Input, XStack, YStack } from 'tamagui';
+import { PrimaryButton } from '../Display/Button';
 
 export function Landing({ goToHome }: { goToHome?: () => void }) {
   return (
     <YStack space="$0.5" style={{ alignItems: 'center' }}>
       <Typography.H1>end</Typography.H1>
       <XStack space="$0.5">
-        <Input padding="$0.5" width="50%" />
-        <Input padding="$0.5" width="50%" />
+        <Input placeholder="Username" padding="$0.5" width="50%" />
+        <Input
+          placeholder="Password"
+          secureTextEntry={true}
+          padding="$0.5"
+          width="50%"
+        />
       </XStack>
-      <Button
-        onPress={() => goToHome?.()}
-        borderRadius={0}
-        width="100%"
-        padding={0}
-      >
-        Login
-      </Button>
+      <PrimaryButton>Login</PrimaryButton>
     </YStack>
   );
 }
