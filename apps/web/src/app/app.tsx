1e95b94425107f41caf89791c480966b3de0ce2c
Joe Badaczewski <joebad@amazon.com>
2024-02-21 11:41:55 -0500

    [mobile + web + libs/wm/*] basic WatermelonDB integration (#37)

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -27,8 +27,10 @@ import {
   Outlet,
   Navigate,
 } from 'react-router-dom';
-import { Home } from '../pages/Home';
+import Home from '../pages/Home';
 import { useAuth } from '@end/auth';
+import { database } from '@end/wm/web';
+import { DatabaseProvider } from '@nozbe/watermelondb/react';
 
 function WithNavigate({
   children,
@@ -149,18 +151,20 @@ export function App() {
 
   return (
     <View style={{ height: '100%', width: '100%' }}>
-      <Providers baseUrl={process.env.API_BASE_URL as string}>
-        <AppRoutes />
-        {/*<SystemDetails*/}
-        {/*  discoverSystem={discoverSystem}*/}
-        {/*  name={name}*/}
-        {/*  setName={setName}*/}
-        {/*  tags={['planetary system']}*/}
-        {/*  h1={Styles.h1}*/}
-        {/*>*/}
-        {/*  <System />*/}
-        {/*</SystemDetails>*/}
-      </Providers>
+      <DatabaseProvider database={database}>
+        <Providers baseUrl={process.env.API_BASE_URL as string}>
+          <AppRoutes />
+          {/*<SystemDetails*/}
+          {/*  discoverSystem={discoverSystem}*/}
+          {/*  name={name}*/}
+          {/*  setName={setName}*/}
+          {/*  tags={['planetary system']}*/}
+          {/*  h1={Styles.h1}*/}
+          {/*>*/}
+          {/*  <System />*/}
+          {/*</SystemDetails>*/}
+        </Providers>
+      </DatabaseProvider>
     </View>
   );
 }
--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,5 +1,42 @@
-import { H1 } from '@end/components';
+import { H1, PrimaryButton } from '@end/components';
+import { useCallback, ComponentType } from 'react';
+import { database } from '@end/wm/web';
+import { Planet } from '@end/wm/core';
+import {
+  compose,
+  withDatabase,
+  withObservables,
+} from '@nozbe/watermelondb/react';
+import { Database, Query } from '@nozbe/watermelondb';
 
-export function Home() {
-  return <H1>Home</H1>;
+function Home({ allPlanets }: { allPlanets: Planet[] }) {
+  const addPlanet = useCallback(async () => {
+    await database.write(async () => {
+      await database.get('planets').create((planet: any) => {
+        planet.name = Math.random().toString();
+      });
+    });
+  }, []);
+
+  return (
+    <>
+      <H1>Home</H1>
+      <ul>
+        {allPlanets.map((planet) => (
+          <li>{planet.name}</li>
+        ))}
+      </ul>
+      <PrimaryButton onPress={addPlanet}>Add planet</PrimaryButton>
+    </>
+  );
 }
+
+export default compose(
+  withDatabase,
+  withObservables(
+    [],
+    ({ database }: { database: Database }): { allPlanets: Query<Planet> } => ({
+      allPlanets: database.get<Planet>('planets').query(),
+    })
+  ) as (arg0: unknown) => ComponentType
+)(Home);
