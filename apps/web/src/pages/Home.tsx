e6c9a3c2a5ef9907c8878004524696b25a5ea7ed
joebad <joe307bad@gmail.com>
2024-03-12 05:22:58 -0400

    [wen] making planet more responsive

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,17 +1,20 @@
-import { Home as H, PrimaryButton } from '@end/components';
+import { Home as H, PrimaryButton, useResponsive } from '@end/components';
 import { database, sync } from '@end/wm/web';
 import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import React, { useState } from 'react';
 import { Hexasphere } from '@end/hexasphere';
+import { useWindowDimensions } from 'react-native';
 
 export default function Home() {
   const [seed, setSeed] = useState(Math.random());
+  const { width } = useWindowDimensions();
+
   return (
     <H database={database} sync={sync} apiUrl={process.env.API_BASE_URL}>
       <Canvas
         style={{ flex: 1, width: '100%' }}
-        camera={{ position: [0, 160, 25], fov: 45 }}
+        camera={{ position: [0, width < 600 ? 300 : 160, 25], fov: 45 }}
       >
         <OrbitControls />
         <Hexasphere key={seed} />
