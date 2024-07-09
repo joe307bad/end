f98af817d67f860d4772e3a7752ce83f515682c0
joebad <joe307bad@gmail.com>
2024-07-08 23:12:31 -0400

    Upgrading mongo deployment

--- deploy/mongo/run.sh
+++ deploy/mongo/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -m
 
-mongodb_cmd="mongod -f /etc/mongod.conf --auth --bind_ip_all --storageEngine wiredTiger"
+openssl rand -base64 756 > /mongodb.key
+
+chmod 600 /mongodb.key
+
+mongodb_cmd="/usr/bin/mongod --replSet rs0 --keyFile /mongodb.key --auth --bind_ip_all --storageEngine wiredTiger"
 cmd="$mongodb_cmd"
 
 $cmd &
