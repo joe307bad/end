853e88a930acb9cd94bd6953504e7d380db86bc0
joebad <joe307bad@gmail.com>
2024-07-20 22:06:46 -0400

    Changes to mongo deploy

--- deploy/mongo/run.sh
+++ deploy/mongo/run.sh
@@ -4,13 +4,14 @@ set -m
 openssl rand -base64 756 > /mongodb.key
 
 chmod 600 /mongodb.key
+chmod -R 600 /var/lib/mongodb/
 
-mongodb_cmd="/usr/bin/mongod --replSet rs0 --keyFile /mongodb.key --auth --bind_ip_all --storageEngine wiredTiger"
+mongodb_cmd="/usr/bin/mongod --replSet rs0 --keyFile /mongodb.key --auth --bind_ip_all --dbpath /var/lib/mongodb/ --storageEngine wiredTiger"
 cmd="$mongodb_cmd"
 
 $cmd &
 
-if [ ! -f /data/db/.mongodb_password_set ]; then
+if [ ! -f /var/lib/mongodb/.mongodb_password_set ]; then
     /set_mongodb_password.sh
 fi
 
