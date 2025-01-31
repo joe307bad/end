4014dc91e3ceeaa76ebba6463d5bf179533c7cf7
joebad <joe307bad@gmail.com>
2025-01-31 00:01:01 -0500

    Trigger web deploy

--- apps/web/src/main-web.tsx
+++ apps/web/src/main-web.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
-// triggering web build
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
